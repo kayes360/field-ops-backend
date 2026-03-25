95f617020e159bd22a113afac9815849d0609206
kayes360 <42603790+kayes360@users.noreply.github.com>
2026-03-25 20:35:19 +0600

    auth-merged

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,39 +1,25 @@
-import {
-  Body,
-  Controller,
-  Get,
-  HttpCode,
-  HttpStatus,
-  NotImplementedException,
-  Post,
-  Request,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, NotImplementedException, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthGuard } from './guards/auth.guard';
+import { PassportLocalGuard } from './guards/passport-local.guard';
+import { PasspoerJwtAuthGuard } from './guards/passport-jwt.guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
 
-  @HttpCode(HttpStatus.OK)
-  @Post('login')
-  login(
-    @Body()
-    input: {
-      name: string;
-      email: string;
-      role: string;
-      password: string;
-    },
-  ) {
-    return this.authService.authenticate(input);
-  }
-  @UseGuards(AuthGuard)
-   @Get('me') 
-    getUserInfo(@Request() request){
-        return request.user;
+    constructor(private authService: AuthService) {}
+
+    @HttpCode(HttpStatus.OK)
+    @Post('login')
+    @UseGuards(PassportLocalGuard)
+    login(@Request() request){
+        return this.authService.signIn(request.user);
     }
-   
- 
+    @Get('me')
+    @UseGuards(PasspoerJwtAuthGuard)
+
+     getUserInfo(@Request() request){
+       
+        return  request.user;
+    }
+
 }
